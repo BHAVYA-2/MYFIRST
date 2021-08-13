53d88f37c7ae3b0d40b5ba05795b699d4e3db37b
BHAVYA-2 <87699179+BHAVYA-2@users.noreply.github.com>
2021-08-13 19:13:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,36 @@
+var ship_1,ship_2,ship_3,ship_4;
+var sea,seaAnimation;
 
 function preload(){
+
+  seaImage = loadAnimation("sea.png");
+  ship = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 
 }
 
 function setup(){
   createCanvas(400,400);
   
+sea = createSprite(200,200,20,20);
+sea.addAnimation("seaImage","sea.png");
+sea.scale=0.4;
+sea.velocityX=-4;
+sea.x -= sea.width/2;
+
+
+ship = createSprite(200,200,20,20);
+ship.addAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+ship.scale = 0.2;
+
 }
 
 function draw() {
-  background("blue");
- 
+  background(220);
+
+if(sea.x > 400){
+  sea.x = sea.width/2;
+}
+
+drawSprites();
+
 }
