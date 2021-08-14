e67dfb1f302b0007a43974ff0447fcc2575e03f3
BHAVYA-2 <87699179+BHAVYA-2@users.noreply.github.com>
2021-08-14 13:48:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,38 @@
-var ship_1,ship_2,ship_3,ship_4;
-var sea,seaAnimation;
+var Runner_1,Runner_2;
+var path,pathAnimation;
 
 function preload(){
 
-  seaImage = loadAnimation("sea.png");
-  ship = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
-
-}
+  //pre-load images
+    pathImage = loadImage("path.png");
+    Runner = loadAnimation("Runner-1.png","Runner-2.png");
+}  
 
 function setup(){
   createCanvas(400,400);
   
-sea = createSprite(200,200,20,20);
-sea.addAnimation("seaImage","sea.png");
-sea.scale=0.4;
-sea.velocityX=-4;
-sea.x -= sea.width/2;
-
-
-ship = createSprite(200,200,20,20);
-ship.addAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
-ship.scale = 0.2;
-
+ //create a path sprite
+  path = createSprite(200,200,20,15);
+  path.addAnimation("pathImage","path.png");
+  path.scale=1.2;
+  path.velocityY = 4;
+  //create a Runner sprite
+  Runner = createSprite(310,360,20,50);
+  Runner.addAnimation("Runner-1.png","Runner-2.png");
+  Runner.scale=0.05;
+ 
 }
 
 function draw() {
   background(220);
 
-if(sea.x > 400){
-  sea.x = sea.width/2;
+  if(path.y > 400){
+    path.y = path.height/2;
+  }
+Runner.x=World.mouseX;
+if(Runner.x<-200){
+  Runner.x = 250;
 }
 
-drawSprites();
-
+  drawSprites();
 }
